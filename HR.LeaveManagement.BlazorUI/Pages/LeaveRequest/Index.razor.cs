c4ce34c78c6767cdac5f63c41e3e9c9e4ccf6a85
Kaushik Rathod <kaushik.kcr@gmail.com>
2024-02-19 20:09:42 +0530

    First code commit

---
+++ HR.LeaveManagement.BlazorUI/Pages/LeaveRequest/Index.razor.cs
@@ -0,0 +1,23 @@
+using global::Microsoft.AspNetCore.Components;
+using HR.LeaveManagement.BlazorUI.Contracts;
+using HR.LeaveManagement.BlazorUI.Models.LeaveRequests;
+
+namespace HR.LeaveManagement.BlazorUI.Pages.LeaveRequest
+{
+    public partial class Index
+    {
+        [Inject] ILeaveRequestService leaveRequestService { get; set; }
+        [Inject] NavigationManager NavigationManager { get; set; }
+        public AdminLeaveRequestViewVM Model { get; set; } = new();
+
+        protected async override Task OnInitializedAsync()
+        {
+            Model = await leaveRequestService.GetAdminLeaveRequestList();
+        }
+
+        void GoToDetails(int id)
+        {
+            NavigationManager.NavigateTo($"/leaverequests/details/{id}");
+        }
+    }
+}
